6d22ed7796bd1c77cf1f07d3b6b60e2974ecefb4
phyche111 <3020118683@qq.com>
2024-05-11 11:15:58 +0800

    lab1 pingpong modefied

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,43 +1,3 @@
-// #include "kernel/types.h"
-// #include "kernel/stat.h"
-// #include "user/user.h"
-
-// int main(void) {
-//   int p[2];
-//   char buf[2];
-//   char* parmsg = "c";
-//   char* chimsg = "d";
-//   pipe(p);
-//   if (fork() == 0) {
-//     if (read(p[0], buf, 1) != 1) {
-//       fprintf(2, "can not read from parent\n");
-//       exit(1);
-//     }
-//     printf("child receive\n", buf[0]);
-//     close(p[0]);
-//     printf("%d:receive ping\n", getpid());
-//     if (write(p[1], chimsg, 1) != 1) {
-//       fprintf(2, "can not write to parent|n");
-//     }
-//     close(p[1]);
-//     exit(0);
-//   } else {
-//     if (write(p[1], parmsg, 1) != 1) {
-//       fprintf(2, "can not write to child|n");
-//       exit(1);
-//     }
-//     wait(0);
-//     if (read(p[0], buf, 1) != 1) {
-//       fprintf(2, "Can't read from child!");
-//       exit(1);
-//     }
-//     printf("parent receive: %c\n", buf[0]);
-//     close(p[0]);
-//     printf("%d: received pong\n", getpid());
-//     exit(0);
-//   }
-// }
-
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
@@ -50,37 +10,22 @@ int main(int argc, char *argv[]){
     pipe(p);
 
     if (fork() == 0){
-        if (read(p[0], buf, 1) != 1){
-            fprintf(2, "Can't read from parent!\n");
-            exit(1);
-        }
+        read(p[0], buf, 1);
         printf("child receive: %c\n", buf[0]);
-        close(p[0]);
+        //close(p[0]);
 
         printf("%d: received ping\n", getpid());
-        if (write(p[1], chimsg, 1) != 1){
-            fprintf(2, "Can't write to parent!");
-        }
-        close(p[1]);
-        
+        write(p[1], chimsg, 1) ;
+        //close(p[1]);
         exit(0);
     }else{
-        if (write(p[1], parmsg, 1) != 1){
-            fprintf(2, "Can't write to child!\n");
-            exit(1);
-        }
-        close(p[1]);
-
+        write(p[1], parmsg, 1);
+        //close(p[1]);
         wait(0);
-
-        if (read(p[0], buf, 1) != 1){
-            fprintf(2, "Can't read from child!");
-            exit(1);
-        }
+        read(p[0], buf, 1);
         printf("parent receive: %c\n", buf[0]);
-        close(p[0]);
+        //close(p[0]);
         printf("%d: received pong\n", getpid());
-
         exit(0);
     }
 }
